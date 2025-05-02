1d427bc7fce4c2fccfcfe69a396fd667059d3e2a
Wan2729 <wanharithazdy12@gmail.com>
2025-05-03 00:49:57 +0800

    3_5_25_0048: Simplify Package1 Main
    Package1 Main is now modify to test the 500x500, 1000x1000 and 5000x5000. Noticable problem is that 5000x5000 may took minutes to be executed.
    
    TODO: Create a function for Timer to be able to write report on the elapsed time

--- src/Version1/Main.java
+++ src/Version1/Main.java
@@ -4,23 +4,44 @@ import Configuration.*;
 public class Main {
     public static void main(String[] args) {
         // Sequential approach
+        Matrix result;
+        Timer timer = new Timer();
+        long elapsedTime;
 
         // 500x500 start
-        Matrix a = new Matrix(3,3);
-        Matrix b = new Matrix(3,3);
-        Matrix result;
-        a.assignRandom();
-        b.assignRandom();
-        System.out.println(a);
-        System.out.println(b);
+        Matrix a1 = new Matrix(500,500);
+        Matrix b1 = new Matrix(500,500);
+        a1.assignRandom();
+        b1.assignRandom();
 
-        Timer timer = new Timer();
         timer.start();
-        result = a.multiplication(b);
-        long elapsedTime = timer.end();
-        System.out.println("Matrix Result\n"+result);
+        result = a1.multiplication(b1);
+        elapsedTime = timer.end();
         System.out.println("Time taken is "+elapsedTime);
         // 500x500 end
 
+        // 1000x1000 start
+        Matrix a2 = new Matrix(1000,1000);
+        Matrix b2 = new Matrix(1000,1000);
+        a2.assignRandom();
+        b2.assignRandom();
+
+        timer.start();
+        result = a2.multiplication(b2);
+        elapsedTime = timer.end();
+        System.out.println("Time taken is "+elapsedTime);
+        // 1000x1000 end
+
+        // 5000x5000 start
+        Matrix a3 = new Matrix(5000,5000);
+        Matrix b3 = new Matrix(5000,5000);
+        a3.assignRandom();
+        b3.assignRandom();
+
+        timer.start();
+        result = a3.multiplication(b3);
+        elapsedTime = timer.end();
+        System.out.println("Time taken is "+elapsedTime);
+        // 5000x5000 end
     }
 }
